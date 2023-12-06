828af1fb110f4acb7616ab4964e0fee375b88680
Josh Pablico <jbpablico27@gmail.com>
2023-12-06 11:12:58 -0600

    JS

--- src/home.js
+++ src/home.js
@@ -1,9 +1,20 @@
 function header() {
-  const content = document.getElementById('content');
-  content.innerHTML = `
-	<h1>Hello World!</h1>
-	<p>Current time: ${new Date()}</p>
+	const content = document.getElementById('content');
+	content.innerHTML = `
+	<header class="header">
+		<h1 class="header_title">Rowena's Cafe</h1>
+		<nav class="header_nav">
+			<ul class="header_nav_list">
+				<li><a href="#" class="header_nav_list_item">Home</a></li>
+				<li><a href="#" class="header_nav_list_item">Menu</a></li>
+				<li><a href="#" class="header_nav_list_item">Contact</a></li>
+				<li><a href="#" class="header_nav_list_item">About</a></li>
+				<li><a href="#" class="header_nav_list_item">Reservation</a></li>
+			</ul>
+		</nav>
+	</header>
   `;
-}	
+}
+
 
 export default header;
--- src/index.js
+++ src/index.js
@@ -11,5 +11,5 @@ const render = () => {
   `;
 }
 
-header(); 
 render();
+header();
