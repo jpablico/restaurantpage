33cd9f1b6c49da840d9be091fd6713ac274442f9
Josh Pablico <jbpablico27@gmail.com>
2023-12-08 12:29:10 -0600

    JS - Working on site prototype first

--- src/home.js
+++ src/home.js
@@ -1,20 +1,15 @@
-function header() {
-	const content = document.getElementById('content');
-	content.innerHTML = `
-	<header class="header">
-		<h1 class="header_title">Rowena's Cafe</h1>
-		<nav class="header_nav">
-			<ul class="header_nav_list">
-				<li><a href="#" class="header_nav_list_item">Home</a></li>
-				<li><a href="#" class="header_nav_list_item">Menu</a></li>
-				<li><a href="#" class="header_nav_list_item">Contact</a></li>
-				<li><a href="#" class="header_nav_list_item">About</a></li>
-				<li><a href="#" class="header_nav_list_item">Reservation</a></li>
-			</ul>
-		</nav>
-	</header>
-  `;
-}
+function home() {
+  const content = document.getElementById('content');
+  content.innerHTML = '';
+  const home = document.createElement('div');
+  home.classList.add('home');
+  const homeTitle = document.createElement('h1');
+  homeTitle.textContent = 'Welcome to our restaurant';
+  const homeText = document.createElement('p');
+  homeText.textContent = 'We are a restaurant that offers a variety of dishes, from the most traditional to the most innovative. We have a wide selection of wines and cocktails. We are located in the heart of the city, so you can visit us whenever you want.';
+  home.appendChild(homeTitle);
+  home.appendChild(homeText);
+  content.appendChild(home);
+};
 
-
-export default header;
+export default home;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import css from './style.css';
 import "./style.css";
-import header from './home';
+import header from './header';
+import home from './home';
 
-const content = document.getElementById('content');
-
-const render = () => {
-  content.innerHTML = `
-	<h1>Hello World!</h1>
-	<p>Current time: ${new Date()}</p>
-  `;
-}
-
-render();
-header();
+//header();
+ 
+const tabs = document.querySelectorAll('.header_nav_list_item');
+tabs.forEach(tab => tab.addEventListener('click', () => {
+  const target = tab.dataset.setId;
+}));
