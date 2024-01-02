181c47e5ee9a2392395841d29046c0840aa15f0a
Josh Pablico <jbpablico27@gmail.com>
2024-01-02 15:28:55 -0600

    Revert "SCSS"
    This reverts commit 73e79c104836de3a1f84f5c7bd066677e8e9a049.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
 import './style.scss';
 
 const contentMain = document.getElementById('content');
-
-document.getElementById('hamburger-menu').addEventListener('click', function() {
-    document.getElementById('menu').classList.toggle('show');
-});
