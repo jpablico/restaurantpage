eedb4f714dfb6a97458996d059376e671c0d88a3
Josh Pablico <jbpablico27@gmail.com>
2024-01-02 18:22:49 -0600

    Navbar JS

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,13 @@
 import '../style.scss';
 
 const contentMain = document.getElementById('content');
+
+function createElement(tag, className, textContent) {
+    const element = document.createElement(tag);
+    if (className) element.className = className;
+    if (textContent) element.textContent = textContent;
+    return element;
+}
+
+
+export { contentMain, createElement}
---
+++ src/scripts/nav.js
@@ -0,0 +1,45 @@
+import { contentMain, createElement } from ".index.js";
+
+function createHeader() {
+	const header = createElement('header', 'header-container');
+
+	const menuDiv = createElement('div', 'header-container__menu');
+	header.appendChild(menuDiv);
+
+	// Menu Left
+	const menuLeftWrapper = createElement('ul', 'menu-left-wrapper');
+	menuDiv.appendChild(menuLeftWrapper);
+
+	const homeLi = createElement('li', 'active', 'HOME');
+	homeLi.dataset.tab = 'home';
+	menuLeftWrapper.appendChild(homeLi);
+
+	const menuLi = createElement('li', null, 'MENU');
+	menuLi.dataset.tab = 'menu';
+	menuLeftWrapper.appendChild(menuLi);
+
+	// Logo
+	const logoDiv = createElement('div');
+	menuDiv.appendChild(logoDiv);
+
+	const logoImg = createElement('img', 'logo-asset');
+	logoImg.src = '/src/assets/KapihanCoLogoDark.svg';
+	logoImg.alt = 'Kapihan Co. logo';
+	logoDiv.appendChild(logoImg);
+
+	// Menu Right
+	const menuRightWrapper = createElement('ul', 'menu-right-wrapper');
+	menuDiv.appendChild(menuRightWrapper);
+
+	const aboutLi = createElement('li', null, 'ABOUT');
+	aboutLi.dataset.tab = 'about';
+	menuRightWrapper.appendChild(aboutLi);
+
+	const contactLi = createElement('li', null, 'CONTACT');
+	contactLi.dataset.tab = 'contact';
+	menuRightWrapper.appendChild(contactLi);
+
+	return header;
+}
+
+export { createHeader }
