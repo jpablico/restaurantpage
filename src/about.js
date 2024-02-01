e86cd86b9ad122d037e5fa5121382660a4d7298f
Josh Pablico <jbpablico27@gmail.com>
2024-02-01 10:26:31 -0600

    JS

---
+++ src/about.js
@@ -0,0 +1,12 @@
+import { contentMain, createElement } from "./index.js";
+
+function createAbout() {
+	const main = createElement('main', 'main-container', null);
+
+	const aboutSection = createElement('section', null, null);
+	aboutSection.id = 'about-section'
+	main.appendChild(aboutSection);
+
+}
+
+export { createAbout }
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -13,18 +13,38 @@ function createElement(tag, className, textContent) {
     return element;
 }
 
+const tabs = document.querySelectorAll('data-tab');
+console.log(tabs);
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const tabName = tab.dataset.tab;
+        const content = document.getElementById('content');
+        while (content.firstChild) {
+            content.removeChild(content.firstChild);
+        }
+        if (tabName === 'home') {
+            content.appendChild(createMain());
+            console.log("Bop");
+        } else if (tabName === 'menu') {
+            content.appendChild(createMenu());
+        } else if (tabName === 'about') {
+            content.appendChild(createAbout());
+        } else if (tabName === 'contact') {
+            content.appendChild(createContact());
+        }
+    });
+});
+
 function appendToContent() {
     const contentDiv = document.getElementById('content');
     if (!contentDiv) {
         console.error('No element with ID "content" found.');
         return;
     }
-    
     contentDiv.appendChild(createHeader());
-    contentDiv.appendChild(createMenu());
-    contentDiv.appendChild(createFooter());
-
 }
 document.addEventListener('DOMContentLoaded', appendToContent);
+
 
 export { contentMain, createElement}
